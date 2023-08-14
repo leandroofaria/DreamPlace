ac8a79b8ac0a391da676da675aeb1e7625b3b4d9
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2023-08-14 20:43:19 -0300

    consertando components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'layout', component: LayoutComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'usuario', component: UsuarioComponent},
+  {path: 'cadastro', component: CadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
